6e3e240d35f56c61adaf822a7e92dafa6d6765fc
kodi venkata keerthan <keerthan1675@gmail.com>
2025-02-12 14:29:21 +0530

    single

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Keerthan Kodi</h1>
+      <p>Email: keerthan1675@gmail.com</p>
+      <p>Phone: +919381997569</p>
+    </div>
+  );
+}
+
+export default App;
+
